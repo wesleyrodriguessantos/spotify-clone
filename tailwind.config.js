fb25581e328637dd527d7f27cdc59814afa7a161
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2021-02-22 09:41:52 -0300

    Estendendo as configurações do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme': "url('/img/bursts.svg')",
+        'spotify-theme-mobile': "url('/img/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    },
   },
   variants: {
     extend: {},
